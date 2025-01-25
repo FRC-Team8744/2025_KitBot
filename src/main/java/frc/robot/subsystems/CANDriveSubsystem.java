2b04f9b6343be2f6cf3e3ed05f5229b30eeefbfc
Alex <Default>
2025-01-25 15:28:10 -0600

    Test changes

--- src/main/java/frc/robot/subsystems/CANDriveSubsystem.java
+++ src/main/java/frc/robot/subsystems/CANDriveSubsystem.java
@@ -9,11 +9,13 @@ import java.util.function.DoubleSupplier;
 import com.revrobotics.RelativeEncoder;
 import com.ctre.phoenix.sensors.PigeonIMU;
 import com.ctre.phoenix.sensors.PigeonIMU;
+import com.revrobotics.spark.ClosedLoopSlot;
 import com.revrobotics.spark.SparkBase.PersistMode;
 import com.revrobotics.spark.SparkBase.ResetMode;
 import com.revrobotics.spark.SparkLowLevel.MotorType;
 import com.revrobotics.spark.SparkMax;
 import com.revrobotics.spark.config.SparkMaxConfig;
+import com.revrobotics.spark.config.ClosedLoopConfig.FeedbackSensor;
 import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
 
 import edu.wpi.first.math.geometry.Pose2d;
@@ -103,9 +105,62 @@ public class CANDriveSubsystem extends SubsystemBase {
     // battery). The current limit helps prevent tripping
     // breakers.
     SparkMaxConfig config = new SparkMaxConfig();
-    config.voltageCompensation(12);
+    // config.voltageCompensation(12);
     config.smartCurrentLimit(DriveConstants.DRIVE_MOTOR_CURRENT_LIMIT);
     config.idleMode(IdleMode.kBrake);
+
+    /*
+    * Configure the encoder. For this specific example, we are using the
+    * integrated encoder of the NEO, and we don't need to configure it. If
+    * needed, we can adjust values like the position or velocity conversion
+    * factors.
+    */
+    config.encoder
+        .positionConversionFactor(1)
+        .velocityConversionFactor(1);
+
+    /*
+    * Configure the closed loop controller. We want to make sure we set the
+    * feedback sensor as the primary encoder.
+    */
+    config.closedLoop
+        .feedbackSensor(FeedbackSensor.kPrimaryEncoder)
+        // Set PID values for position control. We don't need to pass a closed
+        // loop slot, as it will default to slot 0.
+        // .p(0.4)
+        // .i(0)
+        // .d(0)
+        // .outputRange(-1, 1)
+        // Set PID values for velocity control in slot 1
+        .p(kP, ClosedLoopSlot.kSlot1)
+        .i(kI, ClosedLoopSlot.kSlot1)
+        .d(kD, ClosedLoopSlot.kSlot1)
+        .velocityFF(1.0 / 5767, ClosedLoopSlot.kSlot1)
+        .outputRange(-1, 1, ClosedLoopSlot.kSlot1);
+
+    config.closedLoop.maxMotion
+        // Set MAXMotion parameters for position control. We don't need to pass
+        // a closed loop slot, as it will default to slot 0.
+        // .maxVelocity(1000)
+        // .maxAcceleration(1000)
+        // .allowedClosedLoopError(1)
+        // Set MAXMotion parameters for velocity control in slot 1
+        .maxAcceleration(500, ClosedLoopSlot.kSlot1)
+        .maxVelocity(6000, ClosedLoopSlot.kSlot1)
+        .allowedClosedLoopError(1, ClosedLoopSlot.kSlot1);
+
+    /*
+    * Apply the configuration to the SPARK MAX.
+    *
+    * kResetSafeParameters is used to get the SPARK MAX to a known state. This
+    * is useful in case the SPARK MAX is replaced.
+    *
+    * kPersistParameters is used to ensure the configuration is not lost when
+    * the SPARK MAX loses power. This is useful for power cycles that may occur
+    * mid-operation.
+    */
+    // motor.configure(config, ResetMode.kResetSafeParameters, PersistMode.kNoPersistParameters);
+
 
     // Set configuration to follow leader and then apply it to corresponding
     // follower. Resetting in case a new controller is swapped
